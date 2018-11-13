d52797e70d2bcfce130eb1336aee5011907a1efc
Dawid <batek161@gmail.com>
2018-11-13 17:51:28 +0100

    1#Flask

---
+++ zadania.py
@@ -0,0 +1,20 @@
+a = 7.
+for x in range(1,26):
+     if x % 2 == 0:
+        print(x)
+
+lista = [14,"owca","owca2","19",24,18,29,84,9019,True,False,
+		 [1,2,3,4,5],"ojoj jak dużo elemętów ma ta lista","co zorbić"]
+print(lista[7])
+io = [lista[3],lista[4],lista[5],lista[6],lista[7]]
+print(io)
+print( len(lista))
+
+zmienna =	{
+  "brand": "Fore",
+  "model": "Mustang",
+  "year": 1964
+}
+print(zmienna)
+zmienna["model"] = 2018
+print(zmienna["model"])
